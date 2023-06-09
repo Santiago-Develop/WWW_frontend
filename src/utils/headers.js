392e07a4c70d4ed7fd07c97db674f7a199f8ea6d
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2023-06-09 18:23:32 -0500

    changes

--- src/utils/headers.js
+++ src/utils/headers.js
@@ -1,5 +1,6 @@
 /*Creacion de Headers para peticiones*/
 let headersCreado = new Headers();
 headersCreado.append("Content-type", "application/json");
+headersCreado.append("Access-Control-Allow-Origin", "*");
 
 export const headers = headersCreado;
