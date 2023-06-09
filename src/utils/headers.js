9754f3cecfffdf725d00801973ac814c7dadb227
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2023-06-09 18:24:51 -0500

    changes

--- src/utils/headers.js
+++ src/utils/headers.js
@@ -1,6 +1,6 @@
 /*Creacion de Headers para peticiones*/
 let headersCreado = new Headers();
 headersCreado.append("Content-type", "application/json");
-headersCreado.append("Access-Control-Allow-Origin", "*");
+headersCreado.append("Access-Control-Allow-Origin", "https://www-frontend-ihpd.vercel.app/");
 
 export const headers = headersCreado;
