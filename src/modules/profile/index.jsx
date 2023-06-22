f5f028c55f9c6ad5e1bf7c88adeab4032699f651
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2023-06-21 23:05:39 -0500

    loading user data

--- src/modules/profile/index.jsx
+++ src/modules/profile/index.jsx
@@ -3,14 +3,17 @@ import { getUser } from "../../helpers/getUser";
 import moment from "moment/moment";
 import "./style.scss";
 import { getRole } from "../../helpers/getRole";
+import { Spin } from "antd";
 
 export const UserProfile = () => {
   const [data, setData] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const getUserData = async () => {
     const id = localStorage.getItem("id");
     const data = await getUser(id);
     setData(data);
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -28,20 +31,32 @@ export const UserProfile = () => {
             Mi perfil
           </h1>
         </div>
-        <div className="d-flex justify-content-center align-items-center">
-          <div className="d-flex justify-content-center align-items-center flex-column cardProfile">
-            <img
-              src={data?.urlImg ? `data:image/png;base64,${data.urlImg}` : ""}
-              alt="My photo"
-              className="imgProfile"
-            />
-            <span className="name">{data.username}</span>
-            <span className="subfields">{data.email}</span>
-            <span className="subfields">{data.phone}</span>
-            <span className="subfields">{moment(data.birthDate).calendar()}</span>
-            <span className="subfields">{getRole(data.role)}</span>
-          </div>
-        </div>
+
+        {
+          !!loading
+            ? (
+              <Spin size="large" className="m-4" sty>
+                <div className="content" style={{ height: "50px" }} />
+              </Spin>
+            )
+            :
+            (
+              <div className="d-flex justify-content-center align-items-center">
+                <div className="d-flex justify-content-center align-items-center flex-column cardProfile">
+                  <img
+                    src={data?.urlImg ? `data:image/png;base64,${data.urlImg}` : ""}
+                    alt="My photo"
+                    className="imgProfile"
+                  />
+                  <span className="name">{data.username}</span>
+                  <span className="subfields">{data.email}</span>
+                  <span className="subfields">{data.phone}</span>
+                  <span className="subfields">{moment(data.birthDate).calendar()}</span>
+                  <span className="subfields">{getRole(data.role)}</span>
+                </div>
+              </div>
+            )
+        }
       </div>
     </div>
   );
